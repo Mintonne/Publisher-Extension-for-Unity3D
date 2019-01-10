8d19dc9f5340828cc3c2a06a1b19bb5908b1eb5d
Mintonne <39894801+Mintonne@users.noreply.github.com>
2019-01-11 00:10:00 +0300

    Sidebar layout and design completed

--- src/components/TheSidebar.vue
+++ src/components/TheSidebar.vue
@@ -1,35 +1,35 @@
 <template>
   <div id="sidebar">
-    <ul id="sidebar-parent">
-      <li id="dashboard-button" class="sidebarBtn">
+    <ul>
+      <li class="active">
         <i class="material-icons">dashboard</i>
         <p>Dashboard</p>
       </li>
-      <li id="sales-button" class="sidebarBtn">
+      <li>
         <i class="material-icons">attach_money</i>
         <p>Sales</p>
       </li>
-      <li id="downloads-button" class="sidebarBtn">
+      <li>
         <i class="material-icons">get_app</i>
         <p>Downloads</p>
       </li>
-      <li id="revenue-button" class="sidebarBtn">
+      <li>
         <i class="material-icons">credit_card</i>
         <p>Revenue</p>
       </li>
-      <li id="verify-button" class="sidebarBtn">
+      <li>
         <i class="material-icons">verified_user</i>
         <p>Verify Invoice</p>
       </li>
-      <li id="settings-button" class="sidebarBtn">
+      <li>
         <i class="material-icons">settings</i>
         <p>Settings</p>
       </li>
-      <li id="support-button">
+      <li>
         <i class="myicons icon-envelope"></i>
         <p>Support</p>
       </li>
-      <li id="donate-button">
+      <li>
         <i class="myicons icon-paypal"></i>
         <p>Donate</p>
       </li>
@@ -43,7 +43,7 @@ export default {
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 #sidebar {
   position: absolute;
   top: 0;
@@ -52,71 +52,73 @@ export default {
   height: 100%;
   background: #4a4c4f;
   color: #fff;
-  transition: width 300ms ease-in-out;
+  transition: width 200ms ease-in-out;
   z-index: 100;
   box-shadow: 0px 0px 10px #333;
   overflow: hidden;
 
   white-space: nowrap;
   user-select: none;
-}
 
-#sidebar:hover {
-  width: 200px;
-  transition-delay: 100ms;
-}
+  &:hover {
+    width: 200px;
+    transition-delay: 100ms;
+  }
 
-#sidebar.static:hover {
-  width: 60px;
-  transition: none;
-}
+  &.static:hover {
+    width: 60px;
+    transition: none;
+  }
 
-ul {
-  display: table;
-  width: 200px;
-}
+  ul {
+    display: table;
+    width: 200px;
 
-ul > li {
-  display: table-row;
-  cursor: pointer;
+    li {
+      display: table-row;
+      cursor: pointer;
 
-  margin: 0 auto;
-  width: 100%;
-}
+      margin: 0 auto;
+      width: 100%;
 
-ul > li:hover {
-  background-color: #339af0;
-}
+      &:hover {
+        background-color: #339af0;
+      }
 
-ul > li.active {
-  color: #339af0;
-  background-color: #fff;
-}
+      &.active {
+        color: #339af0;
+        background-color: #fff;
+      }
 
-ul > li > * {
-  height: 60px;
-  display: table-cell;
-  vertical-align: middle;
-}
+      > * {
+        height: 60px;
+        display: table-cell;
+        vertical-align: middle;
+      }
 
-ul > li > i {
-  width: 60px;
-  text-align: center;
-}
+      i {
+        width: 60px;
+        text-align: center;
+      }
 
-ul > li > p {
-  text-align: left;
-}
+      p {
+        text-align: left;
+      }
 
-ul > li#support-button {
-  position: absolute;
-  bottom: 60px;
-  width: 200px;
-}
+      &:nth-last-child(1),
+      &:nth-last-child(2) {
+        position: absolute;
+        width: 200px;
+      }
 
-ul > li#donate-button {
-  position: absolute;
-  bottom: 0;
-  width: 200px;
+      &:nth-last-child(1) {
+        bottom: 0;
+      }
+
+      &:nth-last-child(2) {
+        bottom: 60px;
+      }
+    }
+  }
 }
 </style>
