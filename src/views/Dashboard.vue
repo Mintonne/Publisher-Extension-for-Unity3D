a2ee8a0977d75e3872fae69aca0208d992b05658
Mintonne <39894801+Mintonne@users.noreply.github.com>
2019-01-11 21:59:36 +0300

    Added Search Bar

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="section">
     <nav-bar title="Dashboard"></nav-bar>
+    <v-flex xs12 mt-4>
+      <v-text-field v-model="searchText" label="Search" solo single-line clearable>
+      </v-text-field>
+    </v-flex>
+    <p>{{ searchText }}</p>
   </div>
 </template>
 
@@ -8,6 +13,11 @@
 import NavBar from "@/components/TheNavBar.vue";
 
 export default {
+  data: () => {
+    return {
+      searchText: ''
+    }
+  },
   components: {
     NavBar
   }
@@ -15,4 +25,5 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import "@/styles/variables.scss";
 </style>
