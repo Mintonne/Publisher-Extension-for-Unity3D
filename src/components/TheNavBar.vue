014993307852eabf36f47c7f610a4476e3f7b649
Mintonne <39894801+Mintonne@users.noreply.github.com>
2019-01-11 03:22:12 +0300

    Added NavBar component
    Added bootstrap

---
+++ src/components/TheNavBar.vue
@@ -0,0 +1,24 @@
+<template>
+  <ul class="nav justify-content-center">
+    <li class="nav-item">
+      <a class="nav-link disabled">{{ title }}</a>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      default: 'Title Goes Here'
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.nav-link {
+  pointer-events: none;
+}
+</style>
