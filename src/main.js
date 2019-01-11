6d77eb6fe137e1f98385b00829faef18ccdef414
Mintonne <39894801+Mintonne@users.noreply.github.com>
2019-01-11 21:59:20 +0300

    Added Vuetify

--- src/main.js
+++ src/main.js
@@ -2,6 +2,25 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import Vuetify, {
+  VApp,
+  VContainer,
+  VFlex,
+  VTextField,
+  VFadeTransition
+} from 'vuetify/lib'
+
+import 'vuetify/src/stylus/app.styl';
+
+Vue.use(Vuetify, {
+  components: {
+    VApp,
+    VContainer,
+    VFlex,
+    VTextField,
+    VFadeTransition
+  }
+})
 
 new Vue({
   router,
