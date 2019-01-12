89fa995f9ebea2c0702f6459e1395b5c55045af0
Mintonne <39894801+Mintonne@users.noreply.github.com>
2019-01-12 04:44:53 +0300

    Added MDIDX

--- src/main.js
+++ src/main.js
@@ -10,6 +10,7 @@ import Vuetify, {
   VFadeTransition
 } from 'vuetify/lib'
 
+import 'material-design-icons-iconfont/dist/material-design-icons.css';
 import 'vuetify/src/stylus/app.styl';
 
 Vue.use(Vuetify, {
