310246167798f30eca05be645a479b5e6bc2d93b
Mintonne <39894801+Mintonne@users.noreply.github.com>
2019-01-12 19:04:58 +0300

    Fixed icon mixup

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -11,8 +11,8 @@
         <dash-tile name="Downloads" icon="get_app" path="/downloads" :term="searchTerm"></dash-tile>
         <dash-tile name="Revenue" icon="credit_card" path="/revenue" :term="searchTerm"></dash-tile>
         <dash-tile name="Verify Invoice" icon="verified_user" path="/verify" :term="searchTerm"></dash-tile>
-        <dash-tile name="Reviews" icon="trending_up" url="https://www.example.com" :term="searchTerm"></dash-tile>
-        <dash-tile name="Trend Analysis" icon="rate_review" :term="searchTerm"></dash-tile>
+        <dash-tile name="Reviews" icon="rate_review" url="https://www.example.com" :term="searchTerm"></dash-tile>
+        <dash-tile name="Trend Analysis" icon="trending_up" :term="searchTerm"></dash-tile>
         <dash-tile name="Settings" icon="settings" path="/settings" :term="searchTerm"></dash-tile>
       </v-layout>
     </v-container>
