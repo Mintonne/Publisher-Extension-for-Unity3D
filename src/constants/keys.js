f99a70913de18d2ec00ea4dce830f4b947ed2296
Mintonne <39894801+Mintonne@users.noreply.github.com>
2019-01-15 02:48:15 +0300

    Renamed sidebar variable name

--- src/constants/keys.js
+++ src/constants/keys.js
@@ -11,7 +11,7 @@ module.exports = {
   salesSortOrderKey: 'salesSortOrder',
   salesInfoKey: 'salesInfo',
   reviewCountKey: 'reviewCount',
-  animateSidebar: 'animateSidebar',
+  staticSidebarKey: 'staticSidebar',
   openAsPopupKey: 'openAsPopup',
   popupWindowWidthKey: 'popupWindowWidth',
   popupWindowHeightKey: 'popupWindowHeight',
