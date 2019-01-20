56fc31ea659edf2bbd162e8dc483e00f7992f33f
Mintonne <39894801+Mintonne@users.noreply.github.com>
2019-01-20 18:58:56 +0300

    Added save icon

---
+++ src/assets/icons/save.js
@@ -0,0 +1,10 @@
+/* eslint-disable */
+var icon = require('vue-svgicon')
+icon.register({
+  'save': {
+    width: 24,
+    height: 24,
+    viewBox: '0 0 24 24',
+    data: '<path pid="0" d="M15 9H5V5h10m-3 14c-1.66 0-3-1.34-3-3s1.34-3 3-3 3 1.34 3 3-1.34 3-3 3m5-16H5a2 2 0 0 0-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V7l-4-4z"/>'
+  }
+})
