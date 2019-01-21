e0a850e510988e563d8c795cb6cdf2f326cd5c41
Mintonne <39894801+Mintonne@users.noreply.github.com>
2019-01-22 02:51:11 +0300

    Refresh months data on month/year change

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,64 @@
 <template>
   <div data-app="true" id="app">
+    <loader class="fill" v-if="loading" :message="loadingMessage"></loader>
     <sidebar />
     <router-view />
   </div>
 </template>
 
 <script>
+import Api from '@/api';
 import Sidebar from '@/components/TheSidebar.vue';
 import { SharedMethods } from '@/mixins';
 
 export default {
   mixins: [SharedMethods],
+  data() {
+    return {
+      loading: false,
+      loadingMessage: 'Refreshing months data'
+    }
+  },
   created() {
-    this.LoginStatus(this.$store);
+    this.LoginStatus();
+  },
+  mounted() {
+    if (this.$store.getters.getCurrentMonth == null || this.$store.getters.getLastRefresh == null)
+      return;
+
+    let preDate = new Date(this.$store.getters.getCurrentMonth);
+    let curDate = new Date();
+
+    if ((preDate.getMonth() < curDate.getMonth() || preDate.getFullYear() < curDate.getFullYear()) && curDate.getTime() - this.$store.getters.getLastRefresh >= 3600000)
+      this.CacheMonthsData();
+  },
+  methods: {
+    CacheMonthsData() {
+      let id = this.$store.getters.getPubId;
+      let endpoint = `/publisher-info/months/${id}.json`;
+
+      this.loading = true;
+
+      Api.get(endpoint)
+        .then((response) => {
+          let data = response.data;
+
+          let firstMonth = this.InsertCharacter(data.periods[data.periods.length - 1].value, 4, '-');
+          let currentMonth = this.InsertCharacter(data.periods[0].value, 4, '-');
+          let lastRefresh = Date.now();
+
+          this.$store.dispatch('saveMonthsData', { firstMonth, currentMonth, lastRefresh });
+
+          this.SendMessage('restart');
+        })
+        .catch((error) => {
+          console.log(error);
+          this.RequestError();
+        })
+        .then(() => {
+          this.loading = false;
+        });
+    }
   },
   components: {
     Sidebar
