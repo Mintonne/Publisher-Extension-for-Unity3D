93960203209ec1231d113696195cebde8fd14c15
Mintonne <39894801+Mintonne@users.noreply.github.com>
2019-01-23 10:08:56 +0300

    Added info tile & carousel components

---
+++ src/components/InfoCarousel.vue
@@ -0,0 +1,18 @@
+<template>
+  <carousel
+    :autoplay="true"
+    :autoplayTimeout="5000"
+    :loop="true"
+    :navigationEnabled="true"
+    :navigationClickTargetSize="20"
+    :paginationEnabled="false"
+    :perPage="3">
+    <slot></slot>
+  </carousel>
+</template>
+
+<style>
+.VueCarousel-navigation button.VueCarousel-navigation-button:focus {
+  outline: none;
+}
+</style>
---
+++ src/components/InfoTile.vue
@@ -0,0 +1,50 @@
+<template>
+  <v-card class="text-xs-center" elevation="3" height="90">
+    <v-card-title>{{ title }}</v-card-title>
+    <v-card-text :style="{color}">{{ prefix }}{{ text }}</v-card-text>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+    prefix: {
+      type: String,
+      default: '$'
+    },
+    text: {
+      type: String,
+      required: true
+    },
+    color: {
+      type: String,
+      default: '#339af0'
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import "@/styles/variables.scss";
+
+.v-card {
+  margin: 5px;
+
+  .v-card__title {
+    display: block;
+    margin: 0 auto;
+    color: $dark;
+    font-weight: 500;
+    padding: 8px;
+  }
+
+  .v-card__text {
+    font-size: 25px;
+    padding: 8px;
+  }
+}
+</style>
