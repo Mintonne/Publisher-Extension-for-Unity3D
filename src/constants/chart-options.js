cd8db4ee7796025f1b0c6cc5b5e8f0c4aec91fc4
Mintonne <39894801+Mintonne@users.noreply.github.com>
2019-01-23 10:04:27 +0300

    Move to constants folder
    * Added months abbreviations array

--- src/constants/chart-options.js
+++ src/constants/chart-options.js
@@ -1,7 +1,9 @@
 const chartColors = ['#F15854', '#5DA5DA', '#FAA43A', '#60BD68', '#F17CB0', '#B2912F', '#B276B2', '#DECF3F', '#4E49D8', '#4D4D4D'];
 const monthsNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+const monthsAbbreviations = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'July', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'];
 
 module.exports = {
   chartColors,
-  monthsNames
+  monthsNames,
+  monthsAbbreviations
 }
