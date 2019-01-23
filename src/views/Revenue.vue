cc3532f0322e540ebd422595a9c3985702c0f40f
Mintonne <39894801+Mintonne@users.noreply.github.com>
2019-01-23 10:09:37 +0300

    Added revenue section

--- src/views/Revenue.vue
+++ src/views/Revenue.vue
@@ -1,24 +1,222 @@
 <template>
   <div class="section">
+    <loader class="fill" v-if="loading" :message="loadingMessage"></loader>
+
     <nav-bar title="Revenue"></nav-bar>
+
+    <info-carousel v-if="revenueData != null">
+      <slide>
+        <info-tile title="Current Balance" prefix="" :text="currentBalance" />
+      </slide>
+      <slide>
+        <info-tile title="Net Revenue" :text="netRevenue.toFixed(2)" color="#17a2b8" />
+      </slide>
+      <slide>
+        <info-tile title="Total Payout" :text="totalPayout.toFixed(2)" color="#EE2B4B" />
+      </slide>
+      <slide>
+        <info-tile title="Total Commission" :text="totalCommission.toFixed(2)" />
+      </slide>
+      <slide>
+        <info-tile title="Average Revenue" :text="averageRevenue.toFixed(2)" color="#17a2b8" />
+      </slide>
+      <slide>
+        <info-tile title="Total Months" prefix="" :text="totalMonths" color="#EE2B4B" />
+      </slide>
+    </info-carousel>
+
+    <canvas class="mt-3" id="revenueChart" height="320"></canvas>
+
+    <p v-if="revenueData != null" class="mt-3 text-xs-center font-weight-medium fill">* Excluding current month's sales.</p>
+
+    <v-snackbar
+      bottom
+      class="fill"
+      :timeout="0"
+      :value="showSnackbar">
+      Request Error
+      <v-btn
+        color="pink"
+        flat
+        @click="GetRevenueData()">
+        Retry
+      </v-btn>
+    </v-snackbar>
   </div>
 </template>
 
 <script>
-import NavBar from "@/components/TheNavBar.vue";
+import Api from '@/api';
+import NavBar from '@/components/TheNavBar.vue';
+import InfoCarousel from '@/components/InfoCarousel.vue';
+import InfoTile from '@/components/InfoTile.vue';
+import Chart from '@/../node_modules/chart.js/dist/Chart.min.js';
 import { SharedMethods } from '@/mixins';
+import { monthsAbbreviations } from '@/constants/chart-options.js';
 
 export default {
   mixins: [SharedMethods],
   created() {
     if (!this.$store.getters.pubIdStatus)
       this.RedirectToSettings(this.$router);
+
+    this.GetRevenueData();
+  },
+  data() {
+    return {
+      revenueData: null,
+      currentBalance: null,
+      netRevenue: null,
+      totalPayout: null,
+      totalCommission: null,
+      averageRevenue: null,
+      totalMonths: null,
+      showSnackbar: false,
+      loading: false,
+      loadingMessage: 'Fetching revenue data'
+    }
+  },
+  methods: {
+    GetRevenueData() {
+      let id = this.$store.getters.getPubId;
+      let endpoint = `/publisher-info/revenue/${id}.json`;
+
+      this.loading = true;
+      this.showSnackbar = false;
+
+      Api.get(endpoint.trim())
+        .then((response) => {
+          let data = response.data.aaData;
+
+          if (data == null || data.length <= 0) {
+            this.revenueData = null;
+            this.$swal('Error', 'No records found', 'error');
+          }
+          else {
+            this.revenueData = data[0];
+            this.PopulateRevenueData(data);
+          }
+        })
+        .catch((error) => {
+          console.log(error);
+          this.showSnackbar = true;
+        })
+        .then(() => {
+          this.loading = false;
+        });
+    },
+    PopulateRevenueData(data) {
+      let entryDate,
+        chartData = [],
+        chartLabels = [];
+
+      if (data.length > 0) {
+        data.forEach(item => {
+          let Debit = Number(item[2].replace('$', ''));
+          let Credit = Number(item[3].replace('$', ''));
+
+          if (item[1].toLowerCase().includes('revenue for')) {
+            this.netRevenue += Debit;
+            this.netRevenue += Credit;
+
+            if (!item[1].toLowerCase().includes('fixing')) {
+              this.totalMonths++;
+
+              entryDate = new Date('01 ' + item[1].toLowerCase().replace('sale', '').replace('revenue', '').replace('for', '').trim());
+
+              chartData.push(Debit.toFixed(2));
+              chartLabels.push(monthsAbbreviations[entryDate.getMonth()] + ' ' + entryDate.getFullYear());
+
+              if (chartData.length > 6)
+                chartData.shift();
+
+              if (chartLabels.length > 6)
+                chartLabels.shift();
+            }
+          }
+
+          if (item[1].toLowerCase().includes('asset store payout')) {
+            this.totalPayout += Credit * -1;
+            this.totalPayout += Debit * -1;
+          }
+        });
+
+        let payoutRate = Number(this.$store.getters.getPubRate) || 0.7;
+
+        this.currentBalance = data[data.length - 1][4].replace(' ', '');
+        this.totalCommission = (this.netRevenue * (1 - payoutRate)) / payoutRate;
+        this.averageRevenue = this.netRevenue / this.totalMonths;
+
+        this.DrawChart(chartData, chartLabels);
+      }
+    },
+    DrawChart(chartData, chartLabels) {
+      const ctx = document.getElementById('revenueChart').getContext('2d');
+
+      new Chart(ctx, {
+        type: 'line',
+        data: {
+          datasets: [{
+            data: chartData,
+            fill: false,
+            pointBackgroundColor: '#339AF0',
+            borderColor: '#339AF0',
+            pointHitRadius: 20,
+            pointRadius: 5,
+            pointStyle: 'circle',
+            cubicInterpolationMode: 'monotone'
+          }],
+          labels: chartLabels
+        },
+        options: {
+          legend: {
+            display: false
+          },
+          tooltips: {
+            displayColors: false,
+            backgroundColor: '#4A4C4F',
+            bodyFontSize: 16,
+            titleFontSize: 15,
+            callbacks: {
+              title: ([tooltipItems], data) => {
+                return data.labels[tooltipItems.index];
+              },
+              label: (tooltipItem, data) => {
+                return `$${data.datasets[tooltipItem.datasetIndex].data[tooltipItem.index]}`;
+              }
+            }
+          },
+          scales: {
+            yAxes: [{
+              ticks: {
+                beginAtZero: true,
+                fontSize: 14
+              }
+            }],
+            xAxes: [{
+              ticks: {
+                fontSize: 14
+              }
+            }]
+          }
+        }
+      });
+    }
   },
   components: {
-    NavBar
+    NavBar,
+    InfoCarousel,
+    InfoTile
   }
 }
 </script>
 
 <style lang="scss" scoped>
+canvas {
+  width: 90% !important;
+}
+
+.v-snack {
+  margin-left: 60px;
+}
 </style>
