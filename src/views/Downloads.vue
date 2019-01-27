5c1ca520a31cb7cd82124ae1ea6c45552a2f7a5c
Mintonne <39894801+Mintonne@users.noreply.github.com>
2019-01-27 16:10:50 +0300

    Added download section logic

--- src/views/Downloads.vue
+++ src/views/Downloads.vue
@@ -1,11 +1,93 @@
 <template>
   <div class="section">
-    <nav-bar title="Downloads"></nav-bar>
+    <loader class="fill" v-if="loading" :message="loadingMessage"></loader>
+
+    <nav-bar :title="navbarTitle"></nav-bar>
+
+    <v-layout>
+      <v-flex xs10>
+        <v-text-field v-model="searchTerm" label="Search for package" solo single-line>
+        </v-text-field>
+      </v-flex>
+      <v-flex xs2>
+
+        <v-menu
+          :close-on-content-click="false"
+          v-model="showPicker"
+          left
+          full-width
+          transition="slide-x-transition"
+          offset-y>
+          <v-btn
+            block
+            large
+            id="pickerBtn"
+            class=" my-0"
+            slot="activator">
+            <v-icon>$vuetify.icons.calendar</v-icon>
+          </v-btn>
+
+          <v-date-picker
+            color="#339af0"
+            v-model="downloadsPeriod"
+            :min="minDate"
+            :max="maxDate"
+            type="month"
+            :show-current="downloadsPeriod"
+            @input="changeMonth"
+            width="250"
+            no-title
+            scrollable>
+          </v-date-picker>
+
+        </v-menu>
+      </v-flex>
+    </v-layout>
+
+    <info-tile v-if="downloadsData != null && downloadsData.length > 0" prefix="" title="Total Downloads" :text="totalDownloads" />
+
+    <v-layout v-if="downloadsData != null && downloadsData.length > 0" class="px-1">
+      <v-btn class="mx-0 text-none" disabled flat>Packages ({{ sortOptions[currentSortOrder] }})
+      </v-btn>
+      <v-spacer />
+      <v-menu left offset-x>
+        <v-btn class="mx-0 px-2 text-none" flat slot="activator">Sort by
+          <v-icon class="ml-1" right>$vuetify.icons.dropdown</v-icon>
+        </v-btn>
+
+        <v-list dense class="pa-0">
+          <v-list-tile v-for="(option, index) in sortOptions" :key="index" @click="ChangeSortOrder(index)">
+            <v-list-tile-title :class="{'selected-option' : index == currentSortOrder}">{{ option }}</v-list-tile-title>
+          </v-list-tile>
+        </v-list>
+      </v-menu>
+      <v-menu left offset-x>
+        <v-btn class="mx-0 px-2 text-none" flat slot="activator">Order
+          <v-icon class="ml-1" right>$vuetify.icons.dropdown</v-icon>
+        </v-btn>
+
+        <v-list dense class="pa-0">
+          <v-list-tile v-for="(option, index) in orderOptions" :key="index" @click="ChangeOrder(index)">
+            <v-list-tile-title :class="{'selected-option' : index == currentOrder}">{{ option }}</v-list-tile-title>
+          </v-list-tile>
+        </v-list>
+      </v-menu>
+    </v-layout>
+
+    <v-layout v-if="downloadsData != null && downloadsData.length > 0" row wrap>
+      <package-tile v-for="(item, index) in packagesData" :key="index" :packageData="item" :term="searchTerm" downloads />
+    </v-layout>
+
+    <snackbar :value="showSnackbar" :callback="GetDownloadsData" timeout="5000" />
   </div>
 </template>
 
 <script>
+import Api from '@/api';
 import NavBar from "@/components/Navbar.vue";
+import InfoTile from '@/components/InfoTile.vue';
+import PackageTile from '@/components/PackageTile.vue';
+import Snackbar from '@/components/Snackbar.vue';
 import { SharedMethods } from '@/mixins';
 
 export default {
@@ -13,12 +95,170 @@ export default {
   activated() {
     if (!this.$store.getters.pubIdStatus)
       return this.RedirectToSettings(this.$router);
+
+    if (this.downloadsPeriod == null)
+      this.downloadsPeriod = this.$store.getters.getCurrentMonth;
+
+    if (this.downloadsData == null)
+      this.GetDownloadsData();
+  },
+  data() {
+    return {
+      navbarTitle: 'Free Downloads',
+      searchTerm: '',
+      showPicker: false,
+      minDate: this.$store.getters.getFirstMonth,
+      maxDate: new Date().toISOString().substr(0, 10),
+      sortOptions: ['Name', 'Downloads', 'First Download Date', 'Last Download Date'],
+      orderOptions: ['Ascending (A - Z)', 'Descending (Z - A)'],
+      currentSortOrder: this.$store.getters.getDownloadsSortOrder,
+      currentOrder: 1,
+      downloadsData: null,
+      packagesData: [],
+      downloadsPeriod: null,
+      totalDownloads: 0,
+      showSnackbar: false,
+      loading: false,
+      loadingMessage: 'Fetching free downloads data'
+    }
+  },
+  methods: {
+    changeMonth() {
+      this.showPicker = false;
+      this.GetDownloadsData();
+    },
+    GetDownloadsData() {
+      let id = this.$store.getters.getPubId;
+
+      let endpoint = `/publisher-info/downloads/${id}/${this.downloadsPeriod.replace(/-/g, '')}.json`;
+
+      this.loading = true;
+      this.showSnackbar = false;
+
+      Api.get(endpoint.trim())
+        .then((response) => {
+          let data = response.data.aaData;
+
+          this.navbarTitle = new Date(this.downloadsPeriod).toLocaleString(navigator.language, {
+            month: 'long',
+            year: 'numeric'
+          });
+
+          this.downloadsData = data;
+
+          if (data == null) {
+            return this.$swal('Error', 'No records found', 'error');
+          }
+
+          this.PopulateDownloadsData(data);
+        })
+        .catch((error) => {
+          console.log(error);
+          this.showSnackbar = true;
+        })
+        .then(() => {
+          this.loading = false;
+        });
+    },
+    PopulateDownloadsData(data) {
+      if (data.length <= 0)
+        return;
+
+      this.packagesData.splice(0, this.packagesData.length)
+
+      this.totalDownloads = 0;
+
+      data.forEach(item => {
+        this.packagesData.push(item);
+
+        this.totalDownloads += Number(item[1]);
+      });
+
+      this.searchTerm = '';
+      this.SortPackages();
+    },
+    ChangeSortOrder(index) {
+      this.$store.dispatch('saveDownloadsSortOrder', index);
+      this.currentSortOrder = index;
+
+      this.SortPackages();
+    },
+    ChangeOrder(index) {
+      this.currentOrder = index;
+      this.ChangeSortOrder(Number(this.$store.getters.getDownloadsSortOrder));
+
+      this.SortPackages();
+    },
+    SortPackages() {
+      let index = this.currentSortOrder;
+      let desc = this.currentOrder === 0 ? true : false;
+
+      this.packagesData.sort(function (a, b) {
+        let x, y;
+
+        if (index == 1) {
+          x = Number(a[index]);
+          y = Number(b[index]);
+        }
+        else {
+          x = a[index].toUpperCase();
+          y = b[index].toUpperCase();
+        }
+
+        if (desc) {
+          if (x > y) {
+            return 1;
+          }
+          if (x < y) {
+            return -1;
+          }
+          return 0;
+        } else {
+          if (x > y) {
+            return -1;
+          }
+          if (y > x) {
+            return 1;
+          }
+          return 0;
+        }
+      });
+    }
   },
   components: {
-    NavBar
+    NavBar,
+    InfoTile,
+    PackageTile,
+    Snackbar
   }
 }
 </script>
 
 <style lang="scss" scoped>
+@import "@/styles/variables.scss";
+
+.section {
+  overflow-y: auto !important;
+}
+
+.selected-option {
+  color: $primary-color;
+  font-weight: 500;
+}
+
+.v-btn.v-btn--disabled {
+  color: rgba(0, 0, 0, 0.5) !important;
+}
+
+#pickerBtn {
+  height: 48px;
+  margin-left: -1px;
+  background-color: white;
+  color: $primary-color;
+
+  &:hover {
+    background-color: $primary-color;
+    color: white;
+  }
+}
 </style>
