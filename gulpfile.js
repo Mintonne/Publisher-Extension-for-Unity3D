5942c6bb7f6c040d27bad329bdec5327e507504e
Mintonne <39894801+Mintonne@users.noreply.github.com>
2019-01-28 03:47:08 +0300

    Added release task
    * Reorder tasks
    * Removed redudant tasks

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ const run = require('gulp-run');
 const rename = require('gulp-rename');
 const replace = require('gulp-replace');
 const argv = require('yargs').argv;
+const zip = require('gulp-zip');
 
 const paths = {
   chrome_manifest: './src/manifests/manifest-chrome.json',
@@ -18,10 +19,16 @@ gulp.task('set:vendor', function (done) {
   done();
 });
 
-gulp.task('chrome:manifest', function () {
+gulp.task('build', function () {
+  return run(`npm run build --vendor=${argv.vendor}`).exec()
+});
+
+gulp.task('manifest', function () {
   let version = require('./package.json').version;
 
-  return gulp.src(paths.chrome_manifest)
+  let manifest = argv.vendor == 'chrome' ? paths.chrome_manifest : paths.firefox_manifest;
+
+  return gulp.src(manifest)
     .pipe(replace('{{version}}', version))
     .pipe(rename(function (file) {
       file.basename = 'manifest';
@@ -34,12 +41,17 @@ gulp.task('delete:bg-html', function () {
   return run('rm -f dist/background.html').exec()
 });
 
-gulp.task('build', function () {
-  return run(`npm run build --vendor=${argv.vendor}`).exec()
+gulp.task('zip', function () {
+  let fileName = argv.vendor == 'chrome' ? 'chrome-ext' : 'firefox-ext';
+
+  return gulp.src('dist/**/*')
+    .pipe(zip(`${fileName}.zip`))
+    .pipe(gulp.dest('../'))
 });
 
-gulp.task('compile:chrome', gulp.series('set:vendor', 'build', 'chrome:manifest', 'delete:bg-html'));
+gulp.task('compile', gulp.series('set:vendor', 'build', 'manifest', 'delete:bg-html'));
+gulp.task('release', gulp.series('set:vendor', 'build', 'manifest', 'delete:bg-html', 'zip'));
 
-gulp.task('watch:chrome', function () {
-  gulp.watch(['./public/**/*', './src/**/*', 'manifest-chrome.json'], gulp.series('compile:chrome'));
+gulp.task('watch', function () {
+  gulp.watch(['./public/**/*', './src/**/*'], gulp.series('compile'));
 });
